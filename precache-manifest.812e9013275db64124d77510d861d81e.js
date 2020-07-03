ce58f265709c454ab63ca07e3e56178e3edde3e8
abhilash4384 <vickyabhilash4384@gmail.com>
2020-07-03 10:21:25 +0530

    Updates

---
+++ precache-manifest.812e9013275db64124d77510d861d81e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b69bf99f36c7c1eb48df038bbe506c1",
+    "url": "/Corona-Tracker-Webapp/index.html"
+  },
+  {
+    "revision": "bdbb1a2999af1171bf6b",
+    "url": "/Corona-Tracker-Webapp/static/css/main.40f6cf69.chunk.css"
+  },
+  {
+    "revision": "c149c35edd18651a588f",
+    "url": "/Corona-Tracker-Webapp/static/js/2.971a7ffc.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-Tracker-Webapp/static/js/2.971a7ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdbb1a2999af1171bf6b",
+    "url": "/Corona-Tracker-Webapp/static/js/main.01dfec06.chunk.js"
+  },
+  {
+    "revision": "4546642c65e539f27b5c",
+    "url": "/Corona-Tracker-Webapp/static/js/runtime-main.d08cca54.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Corona-Tracker-Webapp/static/media/image.d7265326.png"
+  }
+]);
