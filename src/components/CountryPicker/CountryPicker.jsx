826d1c37afe9ffcab19833f9d2a8401384ecadb6
abhilash4384 <vickyabhilash4384@gmail.com>
2020-07-03 10:23:51 +0530

    Git pages changes added

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -13,7 +13,7 @@ const CountryPicker = ({handleCountryChange}) => {
     };
 
     fetchAPI();
-  }, [setFetechedCountries]);
+  }, []);
 
   return (
     <FormControl className={styles.formControl}>
